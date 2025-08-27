89f377efdb62918789e19d53bc479aad49a3b554
emanuelmadeira <emanuelmadeira28@gmail.com>
2025-08-26 21:06:15 -0300

    declarando as variaveis

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Pega os elementos HTML pelo ID
+const form = document.getElementById("formulario")
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const telefone = document.getElementById("telefone")
+const cpf = document.getElementById("cpf")
+
